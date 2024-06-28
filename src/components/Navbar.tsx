98daa6fb3f686e934b4d0cfcab31a64aa85da9e3
taufir-huda <110535361+taufir-huda@users.noreply.github.com>
2024-06-28 14:48:48 +0600

    upload image page complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
-import { userAgent } from "next/server";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
